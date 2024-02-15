9e12c307fb5d2faebb840ab4022c0f22d1f95e7c
AvotraIsmael <avotraismael4@gmail.com>
2024-02-15 11:01:13 +0100

    IAN:Add authenthication functionality

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,7 +1,26 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func ErrorResponse(c *gin.Context, status int, message string) {
 	c.JSON(status, gin.H{"error": message})
+}
+
+// Hash the password
+const costHash = 14
+
+func HashPassword(password string) (string, error) {
+	if len(password) < 8 {
+		return "", errors.New("password length must be at least 8 characters")
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), costHash)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
 }
